ae74396da6b780f2ef5e29189abe7ba6118f412d
talexcrowell <talexcrowell@gmail.com>
2018-12-21 10:46:55 -0800

    finished dog reducer

---
+++ src/reducers/dogs.js
@@ -0,0 +1,66 @@
+import {
+  FETCH_DOGS_SUCCESS, 
+  FETCH_DOGS_ERROR, 
+  FETCH_DOGS_REQUEST,
+  ADOPT_DOG_SUCCESS,
+  ADOPT_DOG_ERROR,
+  ADOPT_DOG_REQUEST
+} from '../actions/dogs';
+
+const initialState = {
+  data: null,
+  error: null,
+  loading: false
+}
+
+export default function dogReducer(state=initialState, action){
+  if(action.type === FETCH_DOGS_SUCCESS){
+    return{
+      ...state,
+      loading: false,
+      error: null,
+      data: action.dogs
+    }
+  }
+
+  else if(action.type === FETCH_DOGS_ERROR){
+    return{
+      ...state,
+      loading: false,
+      error: action.error,
+      data: null
+    }
+  }
+
+  else if(action.type === FETCH_DOGS_REQUEST){
+    return{
+      ...state,
+      loading: true
+    }
+  }
+
+  else if(action.type === ADOPT_DOG_SUCCESS){
+    return{
+      ...state,
+      loading: false
+    }
+  }
+
+  else if(action.type === ADOPT_DOG_ERROR){
+    return{
+      ...state,
+      loading:false,
+      error: action.error,
+      data:null
+    }
+  }
+
+  else if(action.type === ADOPT_DOG_REQUEST){
+    return{
+      ...state,
+      loading: true
+    }
+  }
+  
+  return state;
+}
